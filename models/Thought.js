1b1e2fd8f5c5cb037247204a98a29c29b72f11bb
AntiqueRised <imjoodie@gmail.com>
2023-07-15 13:24:35 -0500

    added thoughtSchema to Thought model, still need getter method

--- models/Thought.js
+++ models/Thought.js
@@ -4,29 +4,22 @@ const mongoose = require('mongoose');
 
 const handleError = (err) => console.error(err);
 
-//THOUGHTTEXT
-// -string
-// -required
-// -between 1 and 280 chars
-
-//CREADTEDAT
-// -date
-// -set default val to current timestamp
-// -user a getter method to format the timestamp on query
-
-//USERNAME (user that created this thought)
-// - String
-// required
-
-// REACTIONS (replies)
-// - array of nested documents created with the reactionSchema
-
-
-
-// SCHEMA SETTINGS 
-// create a virtual called "reactionCount" that retrieves the length of the thoughts reactions array field on query
-
 const thoughtSchema = new mongoose.Schema({
+    thoughtText: { type: String, required: true, minChar: 1, maxChar: 280 },
+    createdAt: { type: Date, default: Date.now,  },
+    // // use a getter method to format the timestamp on query
+    username: { type: String, required: true, },
+    reactions: [reactionSchema] 
+
+})
+
+
+
+
+
+
+
+const reactionSchema = new mongoose.Schema({
     reactionId: { type: Schema.Types.ObjectId },
     reactionBody: { type: String, required: true, maxChar: 280 },
     username: { type: String, required: true, },
